39daf7162c2658636fae7d5ea178c32aed544b6e
PressureDraper <warmarines00@hotmail.com>
2022-04-24 14:46:06 -0500

    adding new simple route

--- app.js
+++ app.js
@@ -11,3 +11,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+app.get('/launchx', (req, res) => {
+    res.send('Welcome to LaunchX!')
+})
